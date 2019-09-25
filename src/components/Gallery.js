5ed90421c40fe9a9cb03e89bb43c6c3d1428de95
Amadeus <maderamadeus@gmail.com>
2019-09-25 17:38:22 +0200

    updated Image List sorted by birthtime, removed dublicates

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,7 +5,7 @@ import Img from "gatsby-image";
 function Gallery() {
   const ImgQuery = useStaticQuery(graphql`
     query Images {
-      allFile {
+      allFile(sort: {fields: birthtime}) {
         edges {
           node {
             name
