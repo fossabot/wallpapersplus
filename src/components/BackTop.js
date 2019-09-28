5bd22b6b0f35f985cb26c731cadec5136c2dcab3
Amadeus <maderamadeus@gmail.com>
2019-09-28 13:59:54 +0200

    Updated BackTop

--- src/components/BackTop.js
+++ src/components/BackTop.js
@@ -2,10 +2,11 @@ import React from "react";
 
 import { Link } from "gatsby";
 
+
 function BackTop() {
   return (
     <div className="BackTop">
-    <Link to="#anchor_BackTop">
+    <Link href="#anchor_BackTop">
       <button className="mt-8 float-right -mr-16" >
         <span className="bg-blue-700 cursor-pointer float-right m-4 -mr-8 p-6 text-white hover:text-gray-200 text-lg hover:shadow-2xl shadow-lg rounded-full">
           UP
