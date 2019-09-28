97abe46512641886fd6249cce1994f1c9e69a14b
Amadeus <maderamadeus@gmail.com>
2019-09-28 21:50:41 +0200

    build should work again

--- src/components/BackTop.js
+++ src/components/BackTop.js
@@ -1,17 +1,10 @@
 import React from "react";
 
 import { Link } from "gatsby";
-import ScrollMagic from "ScrollMagic";
+
 
 
 function BackTop() {
-  var controller = new ScrollMagic.Controller();
-  new ScrollMagic.Scene({
-		duration: "100%",	// the scene should last for a scroll distance of 100px
-		offset: 150	// start this scene after scrolling for 50px
-	})
-	.setPin("#my-sticky-element") // pins the element for the the scene's duration
-	.addTo(controller); // assign the scene to the controller
   return (
     <div className="BackTop">
     <Link href="/#anchor_BackTop">
